ab82c2ad990123a3bb756a6b4eed6f4be0854378
matthewprince <m.prince.21@unimail.winchester.ac.uk>
2024-03-29 18:19:33 +0000

    modified:   HttpExample/index.js modified:   HttpExample/sample.dat

--- HttpExample/index.js
+++ HttpExample/index.js
@@ -1,25 +1,49 @@
-const crypto = require("crypto");
+const sql = require('mssql');
+
+const sqlConfig = {
+  user: 'azureuser',
+  password: 'waqqly1!',
+  database: 'dogwalker',
+  server: 'waqqlysqlserver.database.windows.net',
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000
+  }
+};
+module.exports = async function (context, req) {
+var walkername = req.query.walkername || req.body.walkername;
+var walkeremail = req.query.walkeremail || req.body.walkeremail;
+var walkerlocation = req.query.walkerlocation || req.body.walkerlocation;
+};
+
+const insertWalker = async (walker) => {
+  try {
+    await sql.connect(sqlConfig);
+    const result = await sql.query("INSERT INTO [dbo].[dogwalkertabletesttwo] (walkername, walkeremail, walkerlocation) VALUES ("+"'"+walkername+"', '"+walkeremail+"', '"+walkerlocation+"');");
+  } catch (err) {
+    console.log("Error inserting walker: ", err);
+  }
+}
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  // const walkername = req.query.walkername || req.body.walkername;
+  // const walkeremail = req.query.walkeremail || req.body.walkeremail;
+  // const walkerlocation = req.query.walkerlocation || req.body.walkerlocation;
+    const walkername = "name"
+    const walkeremail = "email"
+    const walkerlocation = "location"
+    const walker = {
+    walkername, 
+    walkeremail,
+    walkerlocation
+  };
 
-    if (name) {
-        context.bindings.toDoItems = JSON.stringify([{
-            // create a random ID
-            id: crypto.randomUUID(),
-            title: name,
-            completed: false,
-            url: ""
-        }]);
-    }
+  await insertWalker(walker);
+  
+  context.res = {
+    body: "Walker inserted."
+  };
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
 }
